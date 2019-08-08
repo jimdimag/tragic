9bd1a2fa079dfb86c5b15741e34be523df62eb2e
Jim DiMaggio <jimdimag@gmail.com>
2019-08-08 15:47:23 -0400

    added core site files

---
+++ header.php
@@ -0,0 +1,60 @@
+<?php
+include_once 'head.php'
+?>
+<header>
+
+    <div class="topBar">
+        <div class="container">
+            <ul class="top">
+                <li>Up to 25% off + $30 off <span class="grey">rx snow goggle orders of $225 or more. use code:</span>goggleblowout</li>
+                <li>details &RightAngleBracket;&RightAngleBracket;</li>
+                <li>sign in</li>
+                <li>
+                    <img src="images\cart.png">
+                </li>
+            </ul>
+        </div>
+    </div>
+    <nav class="navbar navbar-toggleable-sm navbar-light mb-4">
+
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#main-nav" aria-controls="main-nav" aria-expanded="false" aria-label="Toggle navigation">
+
+            <span class="navbar-toggler-icon"></span>
+
+        </button>
+<div class="container">
+    <!-- <div class="row"> -->
+        <div class="collapse navbar-collapse" id="main-nav">
+            
+                <ul class="navbar-nav">
+                    <li class="topNav">shop by
+                        <ul class="sub-nav ">
+                            <li>sport</li>
+                            <li>category</li>
+                            <li>brand</li>
+                            <li>&nbsp;</li>
+                            <li>men</li>
+                            <li>women</li>
+                            <li>kids</li>
+                        </ul>
+                    </li>
+                    <li class="topNav"> search by
+                        <ul class="sub-nav">
+                            <li>rx</li>
+                            <li>analyzer</li>
+                            <li>help/guides</li>
+                        </ul>
+                    </li>
+                    <li class="topNav">&nbsp;
+                        <ul class="sub-nav">
+                            <li>
+                                <img src="images\search.png">
+                            </li>
+                        </ul>
+                        </div>
+    <!-- </div> -->
+    </div>
+</nav>
+    
+</header>
+<hr>
---
+++ index.php
@@ -0,0 +1,56 @@
+<body>
+<?php
+include_once 'header.php'
+?>
+<div class="logoBar">
+    <div class="container">
+        <div class="row">
+        <div class="col-md-6 col-lg-4 auto">
+            <img src="images/bptw.png">
+        </div>
+        <div class="col-md-6 col-lg-4 auto">
+        <img src="images/sport.png">
+        </div>
+        <div class="col-md-6 col-lg-3 auto">
+        <img src="images/phone.png" class="fl mt-10">
+        <ul class="stack bb fl">
+            <li class="mg">call an optician</li>
+            <li>
+                <a class="orange" href="tel:8888315817">(888) 831 5817</a>
+            </li>
+        </ul>
+        </div>
+    </div>
+    </div>
+</div>
+<div class="nike">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-5 item">
+               <div class="banner">
+                   <img src="images/new.png" class="image">
+               </div> 
+               <div class="desc pl">
+                   <p class="orange bb">all around sport:</p>
+                   <hr>
+                   <p class="bb blue">Skylon ace xv</p>
+               </div>
+               <div class="shop pl hide-small">
+                   <button>
+                       <img src="images/btn2.png" class="image">
+                   </button>
+               </div>
+            </div>
+            <div class="col-md-7">
+                <img src="images/glasses.png" class="image">
+                <div class="shop pl show-small">
+                   <button>
+                       <img src="images/btn2.png" class="image">
+                   </button>
+               </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
